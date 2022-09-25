1f818cd604b07e67b9f1be82938ec4b37c1b78c4
Samina-tenso <tensammi@gmail.com>
2022-09-25 17:43:43 +0200

    changed base name

--- tic-tac-toe/deploy.sh
+++ tic-tac-toe/deploy.sh
@@ -3,8 +3,8 @@ npm run build
 cd dist
 echo > .nojekyll
 git init
-git checkout -b main
+git checkout main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Samina-tenso/Tic-tac-toe.git main:gh-pages
 cd -
