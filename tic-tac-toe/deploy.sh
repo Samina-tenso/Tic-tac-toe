363cd3e2c70fb1ca8cfc810c3a219deb5a7b0519
Samina-tenso <tensammi@gmail.com>
2022-09-25 17:18:20 +0200

    created deploy.sh

---
+++ tic-tac-toe/deploy.sh
@@ -0,0 +1,10 @@
+set -e
+npm run build
+cd dist
+echo > .nojekyll
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+cd -
